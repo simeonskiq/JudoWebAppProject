0b6013cb3bc395f3e226b3cb0ac905ae3c16dd76
Simeon Rachev <simeon226@abv.bg>
2024-11-20 16:32:57 +0200

    Added Judge to EntityValidationConstants

--- JudoApp.Common/EntityValidationConstants.cs
+++ JudoApp.Common/EntityValidationConstants.cs
@@ -17,5 +17,15 @@
             public const int ImageUrlMinLength = 8;
             public const int ImageUrlMaxLength = 2083;
         }
+
+        public static class Judge
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 50;
+            public const int DescriptionMinLenght = 50;
+            public const int DescriptionMaxLenght = 500;
+            public const int ImageUrlMinLength = 8;
+            public const int ImageUrlMaxLength = 2083;
+        }
     }
 }
