790c7cbc988d05d07ce52ea8c78587a7971d7913
Simeon Rachev <simeon226@abv.bg>
2024-11-26 01:59:45 +0200

    Created ErrorViewModel

---
+++ JudoApp.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JudoApp.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
