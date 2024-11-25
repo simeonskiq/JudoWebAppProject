a713b6357d33db6ad071b9710f6083be2464014c
Simeon Rachev <simeon226@abv.bg>
2024-11-26 01:48:46 +0200

    Added ApplicationBuilder And ServiceCollection Extensions

---
+++ JudoApp.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace JudoApp.Web.Infrastructure.Extensions
+{
+    using Data;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+            JudoDbContext dbContext = serviceScope
+                .ServiceProvider
+                .GetRequiredService<JudoDbContext>()!;
+            dbContext.Database.Migrate();
+
+            return app;
+        }
+    }
+}
