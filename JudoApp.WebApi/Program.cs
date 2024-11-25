7530cb203f0e788e73b368bbf1c1ca0f40d0b81e
Simeon Rachev <simeon226@abv.bg>
2024-11-26 01:59:25 +0200

    Updated WebApi Program.cs

--- JudoApp.WebApi/Program.cs
+++ JudoApp.WebApi/Program.cs
@@ -1,20 +1,51 @@
-
 namespace JudoApp.WebApi
 {
+    using Microsoft.EntityFrameworkCore;
+
+    using Data.Models;
+    using JudoApp.Services.Data.Interfaces;
+    using Web.Infrastructure.Extensions;
+    using Services.Mapping;
+    using JudoApp.Web.ViewModels;
+    using JudoApp.Web.Data;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            string connectionString = builder.Configuration.GetConnectionString("SQLServer")!;
 
             // Add services to the container.
+            builder.Services
+                .AddDbContext<JudoDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddCors(cfg =>
+            {
+                cfg.AddPolicy("AllowAll", policyBld =>
+                {
+                    policyBld
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin();
+                });
+            });
+
+            builder.Services.RegisterRepositories(typeof(ApplicationUser).Assembly);
+            builder.Services.RegisterUserDefinedServices(typeof(IClubService).Assembly);
+
             var app = builder.Build();
+
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).Assembly);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -27,6 +58,7 @@ namespace JudoApp.WebApi
 
             app.UseAuthorization();
 
+            app.UseCors("AllowAll");
 
             app.MapControllers();
 
