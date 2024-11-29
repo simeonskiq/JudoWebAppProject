f587b180806d62062e5b2fdb442617529193393e
Simeon Rachev <simeon226@abv.bg>
2024-11-30 01:18:55 +0200

    Updated Base Controller and Added ClubController

--- JudoApp.Web/Controllers/BaseController.cs
+++ JudoApp.Web/Controllers/BaseController.cs
@@ -1,10 +1,19 @@
 ﻿namespace JudoApp.Web.Controllers
 {
-
+    using JudoApp.Services.Data.Interfaces;
+    using JudoApp.Web.Infrastructure.Extensions;
     using Microsoft.AspNetCore.Mvc;
 
+    using static Common.ApplicationConstants;
     public class BaseController : Controller
     {
+        protected readonly IManagerService managerService;
+
+        public BaseController(IManagerService managerService)
+        {
+            this.managerService = managerService;
+        }
+
         protected bool IsGuidValid(string? id, ref Guid parsedGuid)
         {
             // Non-existing parameter in the URL
@@ -21,6 +30,22 @@
             }
 
             return true;
+        }
+
+        protected async Task<bool> IsUserManagerAsync()
+        {
+            string? userId = this.User.GetUserId();
+            bool isManager = await this.managerService
+                .IsUserManagerAsync(userId);
+
+            return isManager;
+        }
+
+        protected async Task AppendUserCookieAsync()
+        {
+            bool isManager = await this.IsUserManagerAsync();
+
+            this.HttpContext.Response.Cookies.Append(IsManagerCookieName, isManager.ToString());
         }
     }
 }
---
+++ JudoApp.Web/Controllers/ClubController.cs
@@ -0,0 +1,211 @@
+﻿namespace JudoApp.Web.Controllers
+{
+
+    using JudoApp.Services.Data.Interfaces;
+    using JudoApp.Web.ViewModels.Club;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ClubController : BaseController
+    {
+        private readonly IClubService clubService;
+        
+        public ClubController(IClubService clubService, IManagerService managerService)
+            : base(managerService)
+        {
+            this.clubService = clubService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<ClubIndexViewModel> club =
+                await this.clubService.IndexGetAllOrderedByNameAsync();
+
+            return this.View(club);
+        }
+
+        [HttpGet]
+        [Authorize]
+#pragma warning disable CS1998
+        public async Task<IActionResult> Create()
+#pragma warning restore CS1998
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(AddClubFormModel model)
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            await this.clubService.AddClubAsync(model);
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string? id)
+        {
+            Guid clubGuid = Guid.Empty;
+            bool isIdValid = this.IsGuidValid(id, ref clubGuid);
+            if (!isIdValid)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            ClubDetailsViewModel? viewModel = await this.clubService
+                .GetClubDetailsByIdAsync(clubGuid);
+
+            // Invalid(non-existing) GUID in the URL
+            if (viewModel == null)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            await this.AppendUserCookieAsync();
+
+            return this.View(viewModel);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Manage()
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            IEnumerable<ClubIndexViewModel> clubs =
+                await this.clubService.IndexGetAllOrderedByNameAsync();
+
+            return this.View(clubs);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Edit(string? id)
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                // TODO: Implement notifications for error and warning messages!
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            Guid clubGuid = Guid.Empty;
+            bool isIdValid = this.IsGuidValid(id, ref clubGuid);
+            if (!isIdValid)
+            {
+                return this.RedirectToAction(nameof(Manage));
+            }
+
+            EditClubFormModel? formModel = await this.clubService
+                .GetClubForEditByIdAsync(clubGuid);
+            if (formModel == null)
+            {
+                return this.RedirectToAction(nameof(Manage));
+            }
+
+            return this.View(formModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Edit(EditClubFormModel formModel)
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return this.View(formModel);
+            }
+
+            bool isUpdated = await this.clubService
+                .EditClubAsync(formModel);
+            if (!isUpdated)
+            {
+                ModelState.AddModelError(string.Empty, "Unexpected error occurred while updating the club! Please contact administrator");
+                return this.View(formModel);
+            }
+
+            return this.RedirectToAction(nameof(Details), "Club", new { id = formModel.Id });
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Delete(string? id)
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            Guid clubGuid = Guid.Empty;
+            if (!this.IsGuidValid(id, ref clubGuid))
+            {
+                return this.RedirectToAction(nameof(Manage));
+            }
+
+            DeleteClubViewModel? clubToDeleteViewModel =
+                await this.clubService.GetClubForDeleteByIdAsync(clubGuid);
+            if (clubToDeleteViewModel == null)
+            {
+                return this.RedirectToAction(nameof(Manage));
+            }
+
+            return this.View(clubToDeleteViewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> SoftDeleteConfirmed(DeleteClubViewModel club)
+        {
+            bool isManager = await this.IsUserManagerAsync();
+            if (!isManager)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            Guid clubGuid = Guid.Empty;
+            if (!this.IsGuidValid(club.Id, ref clubGuid))
+            {
+                return this.RedirectToAction(nameof(Manage));
+            }
+
+            bool isDeleted = await this.clubService
+                .SoftDeleteClubAsync(clubGuid);
+            if (!isDeleted)
+            {
+                TempData["ErrorMessage"] =
+                    "Unexpected error occurred while trying to delete the club! Please contact system administrator!";
+                return this.RedirectToAction(nameof(Delete), new { id = club.Id });
+            }
+
+            return this.RedirectToAction(nameof(Manage));
+        }
+    }
+}
