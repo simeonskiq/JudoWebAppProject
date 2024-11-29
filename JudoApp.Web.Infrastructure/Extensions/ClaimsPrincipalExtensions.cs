7e25e3cfe53a67f2544e444707d422cc89a968fb
Simeon Rachev <simeon226@abv.bg>
2024-11-30 01:16:56 +0200

    Updated AppBuilder and Added Claims Principal Extensions

---
+++ JudoApp.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace JudoApp.Web.Infrastructure.Extensions
+{
+
+    using System.Security.Claims;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal? userClaimsPrincipal)
+        {
+            return userClaimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? null;
+        }
+    }
+}
